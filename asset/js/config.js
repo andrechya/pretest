d1ba4bc8ed2392e614f155830c314525c6b52c18
andrechya <95126199+andrechya@users.noreply.github.com>
2025-12-22 06:48:22 +0700

    Update config.js

--- asset/js/config.js
+++ asset/js/config.js
@@ -5,8 +5,9 @@
 const GLOBAL_CONFIG = {
   // URL Deployment GAS Anda untuk mengakses data API (Menu, Dashboard, Guidance)
   GAS_WEB_APP_URL:
-    "https://script.google.com/macros/s/AKfycbwM1ncwFG4F1PPyySpwCAOmBEQyxUXPSnTQZE64kIkGXNFOLc-tQOb2ZWf3Lz1WFKk1/exec",
+    "https://script.google.com/macros/s/AKfycbzB-Xl_HDrGBwQb5406fYW58TCX_251azjygyDxs1GDnacx51P5W8uzaVlZAJwD68so/exec",
 };
 
 // Pastikan variabel ini tersedia secara global setelah diimpor.
 window.GLOBAL_CONFIG = GLOBAL_CONFIG;
+
